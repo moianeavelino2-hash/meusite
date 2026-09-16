4f525b256f3dec374b5116f24c05c3eec50e0ec2
moianeavelino2-hash <moianeavelino2@gmail.com>
2026-09-16 18:17:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+formContacto.onsubmit = e => {
+    e.preventDefault();
+
+    let ok = true;
+
+    [nome, email, assunto, mensagem].forEach((campo, i) => {
+        let erro = [erroNome, erroEmail, erroAssunto, erroMensagem][i];
+        erro.textContent = campo.value.trim() ? "" : "Campo obrigatório";
+        if (!campo.value.trim()) ok = false;
+    });
+
+    if (ok) {
+        sucesso.textContent = "Mensagem enviada com sucesso!";
+        formContacto.reset();
+    }
+};
